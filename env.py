e28f8a5b0c189d66f8a75333f0b427ab90af5074
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-10-11 16:17:51 +0200

    First version of the snakes gym environment

--- env.py
+++ env.py
@@ -1,10 +1,15 @@
+from typing import List, Tuple
+
 import numpy as np
 from gymnasium import spaces
 from gymnasium.core import ActType, Env
 from gymnasium.envs.registration import register
-from snakes.bot import Bot
-from snakes.bots.random import Random
-from snakes.game import Game
+
+from ...bot import Bot
+from ...bots.random import Random
+from ...constants import Move, MOVES
+from ...game import Game
+from ...snake import Snake
 
 
 class SnakeEnv(Env):
@@ -15,7 +20,7 @@ class SnakeEnv(Env):
 
         self.observation_space = spaces.Dict(
             {
-                'agent': spaces.Box(low=np.array([0, 0]), high=np.array(self.size), dtype=np.float32)
+                'grid': spaces.Box(low=0, high=1, shape=self.size, dtype=np.float32)
             }
         )
 
@@ -24,11 +29,24 @@ class SnakeEnv(Env):
         self.game = None
 
     def step(self, action: ActType):
-        print('step')
+        self.game.agents[0].next_move = MOVES[action]
+        self.game.update()
+
+        if self.game.finished():
+            reward = self.game.scores[0]
+        else:
+            player = next(s for s in self.game.snakes if s.id == 0)
+            opponent = next(s for s in self.game.snakes if s.id == 1)
+            reward = len(player)
+
+        observation = self._get_obs()
+        terminated = self.game.finished()
+        info = self._get_info()
+
+        return observation, reward, terminated, False, info
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
-        print('reset!!!')
 
         agents = {
             0: MockAgent,
@@ -36,9 +54,37 @@ class SnakeEnv(Env):
         }
         self.game = Game(agents=agents)
 
+        return self._get_obs(), self._get_info()
+
+    def _get_obs(self):
+        player = (s for s in self.game.snakes if s.id == 0)
+
+        grid = np.zeros(self.game.grid_size, dtype=np.float32)
+        for snake in self.game.snakes:
+            for segment in snake:
+                grid[segment] = 1
+
+        return {'grid': grid}
+
+    def _get_info(self):
+        return {}
+
 
 class MockAgent(Bot):
-    pass
+    def __init__(self, id: int, grid_size: Tuple[int, int]):
+        self.grid_size = grid_size
+        self.next_move = None
+
+    @property
+    def name(self):
+        return 'MockAgent'
+
+    @property
+    def contributor(self):
+        return 'brammmieee'
+
+    def determine_next_move(self, snake: Snake, other_snakes: List[Snake], candies: List[np.array]) -> Move:
+        return self.next_move
 
 
 register(
